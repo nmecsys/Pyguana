45eae0ecdecb04145b955286178922b59ae60f13
jonatha azevedo <johnmasterchip@gmail.com>
2017-11-14 20:57:21 -0200

    Adicionando mais parâmetros de requisição da API

--- pyguana/iguana_get.py
+++ pyguana/iguana_get.py
@@ -8,7 +8,7 @@ Created on Tue Nov 14 2017
 
 
 
-def iguana_get(token=None, fonte=None, datainicio=None, datafim=None, categoria=None):
+def iguana_get(token=None, fonte=None, datainicio=None, datafim=None, categoria=None, limite=None):
     url_base = 'http://iguana.incertezalab.com/jornais?token='
     assert(token is not None), "É preciso inserir um token valido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
     if fonte is None and datainicio is None and datafim is None and categoria is None:
@@ -16,6 +16,28 @@ def iguana_get(token=None, fonte=None, datainicio=None, datafim=None, categoria=
         dados = url_base + token
         dados_fin = requests.get(dados).json()
 
+    params = []
+    i=1
+    if fonte is not None 
+        parans(i) = "&fonte=" + fonte
+        i = i +1
+    if datainicio is not None 
+        parans(i) = "&datainicio=" + datainicio
+        i = i +1
+        
+    if datafim is not None 
+        parans(i) = "&datafim=" + datafim
+        i = i +1
+    if limite is not None 
+        parans(i) = "&limite=" + limite
+        i = i +1
+    if categoria is not None 
+        parans(i) = "&categoria=" + categoria
+        i = i +1
+    
+    dados = url_base + token + .join(parans)
+    dados_fin = requests.get(dados).json()
+    
     return dados_fin['data']
 
 
