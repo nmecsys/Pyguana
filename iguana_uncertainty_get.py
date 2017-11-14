4cbe3c4d32dea2b0fc850ff2ddc0a68a83f12eb5
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-11-14 15:02:50 -0200

    Uncertainty get updated. Still not ready.

--- iguana_uncertainty_get.py
+++ iguana_uncertainty_get.py
@@ -5,20 +5,25 @@ Created on Tue Nov 14 13:39:58 2017
 @author: fernando.teixeira
 """
 
-import requests
-
-
 token = 'k2rt3f5'
 
-def iguana_get(token=None, fonte=None, datainicio=None, datafim=None, categoria=None):
-    url_base = 'http://iguana.incertezalab.com/jornais?token='
+def iguana_uncertainty_get(token=None, fonte=None, datainicio=None,
+                           datafim=None, categoria=None,
+                           output = ["error","message","data"]):
+    url_base = 'http://iguana.incertezalab.com/incerteza?token='
+    
     assert(token is not None), "É preciso inserir um token valido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
     if fonte is None and datainicio is None and datafim is None and categoria is None:
         
         dados = url_base + token
         dados_fin = requests.get(dados).json()
+        
+    if len(output == 3):
+        
+    else:
+        if("error" in output):
+        elif("message" in output):
+        elif("data" in output):
+        
 
     return dados_fin['data']
-
-
-# teste = iguana_get()
