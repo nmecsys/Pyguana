dde89123890fff434fec5fa60222ae9b2736e9f9
jonatha azevedo <johnmasterchip@gmail.com>
2017-11-14 21:03:23 -0200

    Adicionado mais parametros de requisicao da IGUANA
    Note que para essa instancia da API, temos a variável "NMEC". Ela se refere a classificação feito por algoritmo personalizado baseada em garimpagem de termos no corpo da mensagem. 
    Use "nmec=1" para obter noticias que já foram classificadas como notícia de incerteza econômica" ou "nmec=0" para noticias que não são de incerteza econômica.

--- pyguana/iguana_uncertainty_get.py
+++ pyguana/iguana_uncertainty_get.py
@@ -8,7 +8,7 @@ Created on Tue Nov 14 2017
 token = 'k2rt3f5'
 
 def iguana_uncertainty_get(token=None, fonte=None, datainicio=None,
-                           datafim=None, categoria=None,
+                           datafim=None, categoria=None, nmec=None,
                            output = ["error","message","data"]):
     url_base = 'http://iguana.incertezalab.com/incerteza?token='
     
@@ -18,6 +18,31 @@ def iguana_uncertainty_get(token=None, fonte=None, datainicio=None,
         dados = url_base + token
         dados_fin = requests.get(dados).json()
         
+        
+    params = []
+    i=1
+    if fonte is not None 
+        parans(i) = "&fonte=" + fonte
+        i = i +1
+    if datainicio is not None 
+        parans(i) = "&datainicio=" + datainicio
+        i = i +1
+        
+    if datafim is not None 
+        parans(i) = "&datafim=" + datafim
+        i = i +1
+    if limite is not None 
+        parans(i) = "&limite=" + limite
+        i = i +1
+    if categoria is not None 
+        parans(i) = "&categoria=" + categoria
+        i = i +1
+    if nmec is not None 
+        parans(i) = "&nmec=" + nmec
+        i = i +1    
+    
+    dados = url_base + token + .join(parans)
+    dados_fin = requests.get(dados).json()    
     if len(output == 3):
         
     else:
