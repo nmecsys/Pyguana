9f3cb371f8b513da3e829f26c20e88805b7504a2
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-11-22 13:54:04 -0200

    .

--- pyguana/iguana_uncertainty_get.py
+++ pyguana/iguana_uncertainty_get.py
@@ -19,31 +19,14 @@ def iguana_uncertainty_get(token=None, fonte=None, datainicio=None,
         dados_fin = requests.get(dados).json()
         
         
-    params = []
-
-    if fonte is not None: 
-        params.append("&fonte=" + fonte)
-        
-    if datainicio is not None: 
-        params.append("&datainicio=" + datainicio)
+    if len(output) == 3:
         
         
-    if datafim is not None: 
-        params.append("&datafim=" + datafim)
-
-    if limite is not None: 
-        params.append("&limite=" + limite)
-
-    if categoria is not None: 
-        params.append("&categoria=" + categoria)
-
-    if nmec is not None: 
-        params.append("&nmec=" + nmec)
-
-    
-    #corrigir esse .join
-    dados = url_base + token + .join(str(x) for x in params)
-    dados_fin = requests.get(dados).json()    
+    else:
+        
+        if "error" in output:
+        if "message" in output:
+        if "data" in output:
     
         
         
