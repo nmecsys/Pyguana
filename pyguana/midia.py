83cc1b42b579e60ee874f2a8b478a18003d95153
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-11-22 14:14:47 -0200

    criando função midia.

---
+++ pyguana/midia.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 14:00:35 2017
+
+@author: fernando.teixeira
+"""
+
+
+import pandas as pd
+
+def midia(ano, mes):
+    
+    ## Lendo as séries históricas de mídia
+    ## colocar a série histórica no banco de dados
+    
+    data_arquivo =
+    total = 
+    incert = 
+    
+    ## Valor Econômico online
