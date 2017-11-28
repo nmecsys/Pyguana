3560995f5e8c243e31d29f7b73a033ecf26948b4
fernote7 <fernote7@gmail.com>
2017-11-28 15:34:33 -0200

    .

--- pyguana/Iguana.py
+++ pyguana/Iguana.py
@@ -8,7 +8,7 @@ Created on Tue Nov 23 2017
 
 import pandas as pd
 import requests
-import pyguana
+
 
 
 '''
@@ -52,11 +52,12 @@ class Iguana(object):
         
         url_base = 'http://iguana.incertezalab.com/jornais?token='
         
-        assert(token is not None), "É preciso inserir um token válido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
+        assert(self.token is not None), "É preciso inserir um token válido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
         if fonte is None and datainicio is None and datafim is None and categoria is None:
             
-            dados = url_base + token
-
+            dados = url_base + self.token
+            if dados.empty == True:
+                return "É preciso inserir um token válido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
     
         params = []
     
@@ -79,7 +80,7 @@ class Iguana(object):
         
         
         #corrigir esse .join
-        dados = url_base + token + "".join([str(x) for x in params])
+        dados = url_base + self.token + "".join([str(x) for x in params])
         dados_fin = requests.get(dados).json()
         dados_fin = pd.DataFrame(dados_fin['data'])    
         
@@ -94,10 +95,10 @@ class Iguana(object):
     
         url_base = 'http://iguana.incertezalab.com/incerteza?token='
         
-        assert(token is not None), "É preciso inserir um token valido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
+        assert(self.token is not None), "É preciso inserir um token valido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
         if fonte is None and datainicio is None and datafim is None and categoria is None:
             
-            dados = url_base + token
+            dados = url_base + self.token
             dados_fin = requests.get(dados).json()
             
         return dados_fin['data']
@@ -111,7 +112,7 @@ class Iguana(object):
         # start='2010-01-01'
         # end='2011-03-01'
     
-        assert(token is not None), "É preciso inserir um token válido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
+        assert(self.token is not None), "É preciso inserir um token válido! \n Solicite em www.iguana.incertezalab.com/documentation/index.php"
         assert(start or end is None), "É necessario passar a janela de data das notícias!" 
         
         ##- valor online
@@ -119,28 +120,28 @@ class Iguana(object):
         valor_online = pyguana.leitor(valor_online, start_date=start,end_date=end)
         
         ##- valor impresso    
-        valor_impresso = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = "2012",datafim = "2015",fonte = "Valor_impresso"))['noticia']
+        valor_impresso = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = "2012",datafim = "2015",fonte = "Valor_impresso"))['noticia']
         valor_impresso = pyguana.leitor(valor_impresso,start_date = start,end_date=end)
         ##- folha online
-        folha_online = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "Folha_online"))['noticia']
+        folha_online = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "Folha_online"))['noticia']
         folha_online = pyguana.leitor(folha_online,start_date = start,end_date=end)
         ##- folha impresso
-        folha_impresso = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "Folha_impresso"))['noticia']
+        folha_impresso = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "Folha_impresso"))['noticia']
         folha_impresso = pyguana.leitor(folha_impresso,start_date = start,end_date=end)
         ##- @estadao
-        estadao  = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "@estadao"))['noticia']
+        estadao  = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "@estadao"))['noticia']
         estadao = pyguana.leitor(estadao,start_date = start,end_date=end)
         ##- @correio
-        correio  = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "@correio"))['noticia']
+        correio  = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "@correio"))['noticia']
         correio = pyguana.leitor(correio,start_date = start,end_date=end)
         ##- @oglobo
-        oglobo   = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "@oglobo"))['noticia']
+        oglobo   = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "@oglobo"))['noticia']
         oglobo = pyguana.leitor(oglobo,start_date = start,end_date=end)
         ##- @zerohora
-        zerohora = pd.DataFrame(pyguana.Iguana.get(token=token,datainicio = start,datafim = end,fonte = "zehora"))['noticia']
+        zerohora = pd.DataFrame(pyguana.Iguana.get(self.token,datainicio = start,datafim = end,fonte = "zehora"))['noticia']
         zerohora = pyguana.leitor(zerohora,start_date = start,end_date=end)
         
         return valor_online
 
 
-token = 'k2rt3f5'
+
