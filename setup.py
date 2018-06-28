a42f3b7c57afbab5fc9f72e27a9fbad7f7d65667
Jonatha Azevedo da Costa <jonatha.costa@fgv.br>
2018-06-28 11:34:35 -0300

    up

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(name='pyguana',
-      version='0.1',
+      version='0.1.5',
       description='The pyguana provides a python wrapper to the Iguana API.',
       long_description=open('README.rst').read(),
       url='http://github.com/nmecsys/pyguana',
-      author='Fernando Teixeira',
-      author_email='fernando.teixeira@fgv.br',
+      author='Jonatha Costa','Fernando Teixeira',
+      author_email='jonatha.costa@fgv.br','fernandoteixeira@bancobbm.com.br',
       license='BSD',
       packages=['pyguana'],
-	  install_requires=['requests', 'pandas'],
+	  install_requires=['requests', 'pandas','json'],
       zip_safe=False)
